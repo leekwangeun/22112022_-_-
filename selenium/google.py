0f4e54b48386b7ac03aca89d557c8b4df56b232a
leekwangeun <62818315+leekwangeun@users.noreply.github.com>
2023-06-09 23:50:23 +0900

    Delete google.py

--- selenium/google.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
- 
-driver = webdriver.Chrome(executable_path='C:/Users/lke67/Desktop/소설 폴더/selenium/chromedriver.exe')
-driver.get("http://www.google.com/")
